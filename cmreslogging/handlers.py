84ec82525423b71058a6db2e837d4de2436c877d
HWJ <hwjfordev@gmail.com>
2019-03-19 19:33:56 +0800

    fix Log dropping issue
    object mapping for [args] tried to parse field [null] as object, but found a concrete value

--- cmreslogging/handlers.py
+++ cmreslogging/handlers.py
@@ -328,8 +328,8 @@ class CMRESHandler(logging.Handler):
         rec = self.es_additional_fields.copy()
         for key, value in record.__dict__.items():
             if key not in CMRESHandler.__LOGGING_FILTER_FIELDS:
-                if key == "args":
-                    value = tuple(str(arg) for arg in value)
+                if key == "args" and type(value) is tuple:
+                    value = { 'a'+str(i):v for i, v in enumerate(value)}
                 rec[key] = "" if value is None else value
         rec[self.default_timestamp_field_name] = self.__get_es_datetime_str(record.created)
         with self._buffer_lock:
